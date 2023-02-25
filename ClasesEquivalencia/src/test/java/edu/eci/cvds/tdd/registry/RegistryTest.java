67c3a1dc1fa199498a6ae802145896dff5b50b33
Mariana <marianapulido075@gmail.com>
2023-02-25 11:23:07 -0500

    Ultimo Ejercicio

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -7,51 +7,51 @@ public class RegistryTest {
 
     @Test
     public void validateRegistryResult() {
-
+        // Arrange
         Person person = new Person("Mariana",1193566441,20,Gender.FEMALE,true);
-
+        // Act
         RegisterResult result = registry.registerVoter(person);
-
+        // Assert
         Assert.assertEquals(RegisterResult.VALID, result);
     }
     @Test
     public void ValidarDuplicado() {
-
+        // Arrange
         Person person = new Person("Mariana",1193566441,20,Gender.FEMALE,true);
-
+        // Act
         RegisterResult result1 = registry.registerVoter(person);
         RegisterResult result = registry.registerVoter(person);
-
+        // Assert
         Assert.assertEquals(RegisterResult.DUPLICATED, result);
     }
 
     @Test
     public void ValidarEstaMuerto() {
-
+        // Arrange
         Person person = new Person("Julio",79400552,75,Gender.MALE,false);
-
+        // Act
         RegisterResult result = registry.registerVoter(person);
-
+        // Assert
         Assert.assertEquals(RegisterResult.DEAD, result);
     }
 
     @Test
     public void ValidaEsMenordeEdad() {
-
+        // Arrange
         Person person = new Person("Juanita",1141517655,14,Gender.FEMALE,true);
-
+        // Act
         RegisterResult result = registry.registerVoter(person);
-
+        // Assert
         Assert.assertEquals(RegisterResult.UNDERAGE, result);
     }
 
     @Test
     public void ValidarEdadIncorrecta() {
-
+        // Arrange
         Person person = new Person("Antonio",78947,-13,Gender.MALE,true);
-
+        // Act
         RegisterResult result = registry.registerVoter(person);
-
+        // Assert
         Assert.assertEquals(RegisterResult.INVALID_AGE, result);
     }
 
